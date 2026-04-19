48fe7e00b901d82844c7bb1db1a560602a2b5d60
mg15best <migangpower@hotmail.com>
2026-04-19 21:51:05 +0100

    Remove unnecessary blank line in ejercicio2_inferencia.py

--- ejercicio2_inferencia.py
+++ ejercicio2_inferencia.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
